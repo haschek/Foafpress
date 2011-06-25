6d9e76ea78cc0d6f1b03f84d45b9c7aa1c120112
Sebastian Tramp <mail@sebastian.tramp.name>
2011-06-25 14:13:49 +0200

    [add] page content as sioc:content or content:encoded

--- foafpressapp/core/Foafpress.config.php
+++ foafpressapp/core/Foafpress.config.php
@@ -2,26 +2,28 @@
 
 // -- Namespaces ---------------------------------------------------------------
 
-$c['ns'] = array(
-	'admin'=>'http://webns.net/mvcb/',
-	'air'=>'http://www.daml.org/2001/10/html/airport-ont#',
-	'bio'=>'http://purl.org/vocab/bio/0.1/',
-	'cert'=>'http://www.w3.org/ns/auth/cert#',
-	'con'=>'http://www.w3.org/2000/10/swap/pim/contact#',
-	'dc'=>'http://purl.org/dc/elements/1.1/',
-	'doap'=>'http://usefulinc.com/ns/doap#',
-    'foaf'=>'http://xmlns.com/foaf/0.1/',
-	'geo'=>'http://www.w3.org/2003/01/geo/wgs84_pos#',
-	'owl'=>'http://www.w3.org/2002/07/owl#',
-	'rdf'=>'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
-	'rdfs'=>'http://www.w3.org/2000/01/rdf-schema#',
-	'rel'=>'http://purl.org/vocab/relationship/',
-	'rsa'=>'http://www.w3.org/ns/auth/rsa#',
-	'rss'=>'http://purl.org/rss/1.0/',
-	'sioc'=>'http://rdfs.org/sioc/ns#',
-	'vcard'=>'http://www.w3.org/2006/vcard/ns#',
-	'cv'=>'http://kaste.lv/~captsolo/semweb/resume/cv.rdfs#',
-	'ov'=>'http://open.vocab.org/terms/'
+$c['ns']      = array(
+    'admin'   => 'http://webns.net/mvcb/',
+    'air'     => 'http://www.daml.org/2001/10/html/airport-ont#',
+    'bio'     => 'http://purl.org/vocab/bio/0.1/',
+    'cert'    => 'http://www.w3.org/ns/auth/cert#',
+    'con'     => 'http://www.w3.org/2000/10/swap/pim/contact#',
+    'dc'      => 'http://purl.org/dc/elements/1.1/',
+    'doap'    => 'http://usefulinc.com/ns/doap#',
+    'foaf'    => 'http://xmlns.com/foaf/0.1/',
+    'geo'     => 'http://www.w3.org/2003/01/geo/wgs84_pos#',
+    'owl'     => 'http://www.w3.org/2002/07/owl#',
+    'rdf'     => 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+    'rdfs'    => 'http://www.w3.org/2000/01/rdf-schema#',
+    'rel'     => 'http://purl.org/vocab/relationship/',
+    'rsa'     => 'http://www.w3.org/ns/auth/rsa#',
+    'rss'     => 'http://purl.org/rss/1.0/',
+    'sioc'    => 'http://rdfs.org/sioc/ns#',
+    'vcard'   => 'http://www.w3.org/2006/vcard/ns#',
+    'cv'      => 'http://kaste.lv/~captsolo/semweb/resume/cv.rdfs#',
+    'ov'      => 'http://open.vocab.org/terms/',
+    'content' => 'http://purl.org/rss/1.0/modules/content/',
+    'dbpedia' => 'http://dbpedia.org/resource/'
 );
 
 // -- Supported HTTP Methods ---------------------------------------------------
