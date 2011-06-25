4ff7a60aabf09d83897ecb117241d2679046ec44
Michael Haschke <haschek@eye48.com>
2011-06-25 15:01:36 +0200

    [fix] delete ! char from the beginning a command

--- foafpressapp/core/templates/Foafpress.html.php
+++ foafpressapp/core/templates/Foafpress.html.php
@@ -1,9 +1,9 @@
 <?php
 
-!$this->cache->recordOutput();
+$this->cache->recordOutput();
 
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <?php
         // add some folder uris
