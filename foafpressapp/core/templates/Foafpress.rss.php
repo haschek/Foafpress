39d46dbfb7a087766116c70a214559264ec6f022
Michael Haschke <haschek@eye48.com>
2011-09-24 00:25:49 +0200

    [fix] using new output cache name in rss layout template

--- foafpressapp/core/templates/Foafpress.rss.php
+++ foafpressapp/core/templates/Foafpress.rss.php
@@ -28,5 +28,5 @@ echo '<?xml version="1.0" encoding="UTF-8"?>'.PHP_EOL;
 </rss>
 
 <?php
-$this->cache->saveOutput($this->file.$this->pm->Foafpress->languageStackPreferences.$this->pm->Foafpress->extensiontype);
+$this->cache->saveOutput($this->pm->Foafpress->filename, 'FoafpressOutput_'.str_replace(',', '_', $this->pm->Foafpress->languageStackPreferences));
 ?>
